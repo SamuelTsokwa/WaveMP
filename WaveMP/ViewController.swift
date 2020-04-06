11e5220e837461edc10f59d0acd441c3edbc0e5b
Samuel Tsokwa <samueltsokwa@yahoo.com>
2020-04-06 00:07:04 -0700

    Initial Commit

---
+++ WaveMP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WaveMP
+//
+//  Created by Samuel Tsokwa on 2020-04-06.
+//  Copyright © 2020 Samuel Tsokwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
